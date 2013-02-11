2f03d323da92ba0c18b74a9e35292c88a663dbf3
Evan Layman <evan_layman@berkeley.edu>
2013-02-11 15:09:12 -0800

    updated project

--- test/users_model.js
+++ test/users_model.js
@@ -5,37 +5,37 @@ var assert = require('assert')
 tests = {
   'Test Model Add': function () {
   	UsersModel.add('Greg', 'MyPassword!', function (answerDict) {
-  		assertEqual(answerDict, {'errCode': 1, 'count': 1});
+  		assert.deepEqual(answerDict, {'errCode': 1, 'count': 1});
   	});
   },
   'Test Model Add Same': function () {
 	UsersModel.add('Greg', 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': -2});
+  		assert.deepEqual(answerDict, {'errCode': -2});
   	});
   },
   'Test Model Add Different': function () {
 	UsersModel.add('Greg2', 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': 1, 'count': 1});
+  		assert.deepEqual(answerDict, {'errCode': 1, 'count': 1});
   	});
   },
   'Test Model Add Empty Username': function () {
 	UsersModel.add('', 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': -3});
+  		assert.deepEqual(answerDict, {'errCode': -3});
   	});
   },
   'Test Model Add Null Username': function () {
 	UsersModel.add(null, 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': -3});
+  		assert.deepEqual(answerDict, {'errCode': -3});
   	});
   },
   'Test Model Add 129 Username': function () {
 	UsersModel.add('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': -3});
+  		assert.deepEqual(answerDict, {'errCode': -3});
   	});
   },
   'Test Model Add Empty Password': function () {
 	UsersModel.add('Greg5', '', function (answerDict) {
-  		assert.equal(answerDict, {"errCode": -4});
+  		assert.deepEqual(answerDict, {"errCode": -4});
   	});
   },
   'Test Model Add Empty Password': function () {
@@ -43,7 +43,7 @@ tests = {
 		for (var key in answerDict){
   			console.log(key + " : " + answerDict[key]);
   		}
-	  		assert.equal(answerDict, {"errCode": -4});
+	  		assert.deepEqual(answerDict, {"errCode": -4});
 	  	});
   },
   'Test Model Add 129 Password': function () {
@@ -51,7 +51,7 @@ tests = {
   		for (var key in answerDict){
   			console.log(key + " : " + answerDict[key]);
   		}
-  		assert.equal(answerDict, {"errCode": -4});
+  		assert.deepEqual(answerDict, {"errCode": -4});
   	});
   }
  //  'Test Model Login 10 times': function () {
