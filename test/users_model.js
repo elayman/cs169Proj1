cbcbe3f2d27688c1a5f05f0c4afdc4567731c753
Evan Layman <evan_layman@berkeley.edu>
2013-02-11 15:08:08 -0800

    updated project

--- test/users_model.js
+++ test/users_model.js
@@ -5,38 +5,38 @@ var assert = require('assert')
 tests = {
   'Test Model Add': function () {
   	UsersModel.add('Greg', 'MyPassword!', function (answerDict) {
-  		assert.equal(answerDict, {'errCode': 1, 'count': 1});
+  		assertEqual(answerDict, {'errCode': 1, 'count': 1});
   	});
   },
   'Test Model Add Same': function () {
 	UsersModel.add('Greg', 'MyPassword!', function (answerDict) {
-	  		assert.equal(answerDict, {'errCode': -2});
-	  	});
+  		assert.equal(answerDict, {'errCode': -2});
+  	});
   },
   'Test Model Add Different': function () {
 	UsersModel.add('Greg2', 'MyPassword!', function (answerDict) {
-	  		assert.equal(answerDict, {'errCode': 1, 'count': 1});
-	  	});
+  		assert.equal(answerDict, {'errCode': 1, 'count': 1});
+  	});
   },
   'Test Model Add Empty Username': function () {
 	UsersModel.add('', 'MyPassword!', function (answerDict) {
-	  		assert.equal(answerDict, {'errCode': -3});
-	  	});
+  		assert.equal(answerDict, {'errCode': -3});
+  	});
   },
   'Test Model Add Null Username': function () {
 	UsersModel.add(null, 'MyPassword!', function (answerDict) {
-	  		assert.equal(answerDict, {'errCode': -3});
-	  	});
+  		assert.equal(answerDict, {'errCode': -3});
+  	});
   },
   'Test Model Add 129 Username': function () {
 	UsersModel.add('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'MyPassword!', function (answerDict) {
-	  		assert.equal(answerDict, {'errCode': -3});
-	  	});
+  		assert.equal(answerDict, {'errCode': -3});
+  	});
   },
   'Test Model Add Empty Password': function () {
 	UsersModel.add('Greg5', '', function (answerDict) {
-	  		assert.equal(answerDict, {"errCode": -4});
-	  	});
+  		assert.equal(answerDict, {"errCode": -4});
+  	});
   },
   'Test Model Add Empty Password': function () {
 	UsersModel.add('Greg6', null, function (answerDict) {
@@ -48,11 +48,11 @@ tests = {
   },
   'Test Model Add 129 Password': function () {
 	UsersModel.add('Greg7', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', function (answerDict) {
-	  		for (var key in answerDict){
-	  			console.log(key + " : " + answerDict[key]);
-	  		}
-	  		assert.equal(answerDict, {"errCode": -4});
-	  	});
+  		for (var key in answerDict){
+  			console.log(key + " : " + answerDict[key]);
+  		}
+  		assert.equal(answerDict, {"errCode": -4});
+  	});
   }
  //  'Test Model Login 10 times': function () {
  //  	// var x = 2;
